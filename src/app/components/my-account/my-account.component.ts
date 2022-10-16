29fbf948c639d2bb3b11dc8af5d00221290f2aaa
unknown <moaadakdi123@gmail.com>
2022-10-16 20:36:55 +0100

    moaad commit

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -4,58 +4,58 @@ import { from, Subscription } from 'rxjs';
 import { LoginService } from '../../services/login.service';
 import { TokenStorageService } from '../../services/token-storage.service';
 import { __values } from 'tslib';
-import {FormGroup,FormControl} from '@angular/forms'
+import { FormGroup, FormControl } from '@angular/forms';
 import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.css']
+  styleUrls: ['./my-account.component.css'],
 })
 export class MyAccountComponent implements OnInit {
-
-  loginSubscription : Subscription | any ;
-  isLoginSuccessful: boolean | null = null   ;
+  loginSubscription: Subscription | any;
+  isLoginSuccessful: boolean | null = null;
   loginForm!: FormGroup;
   constructor(
-    private loginService : LoginService,
-    private tokenService : TokenStorageService,
-    private route : Router,
-    private dataSharingService : DataSharingService
-  ) { }
+    private loginService: LoginService,
+    private tokenService: TokenStorageService,
+    private route: Router,
+    private dataSharingService: DataSharingService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      username:new FormControl(''),
-      password:new FormControl(''),
-    })
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
-  loginAction(myForm : FormGroup){
+  loginAction(myForm: FormGroup) {
     const loginValues = {
-      username: myForm.value.username+"",
-      password: myForm.value.password+""
-    }
-    this.loginSubscription = this.loginService.login(loginValues.username,loginValues.password).subscribe(
-      (value : any) =>{
-        this.tokenService.saveToken(value.token) ;
-        const userConnected =  {
-          id: value.id,
-          email: value.email,
-          firstName: value.firstName,
-          lastName: value.lastName,
-          gender: value.gender,
-          username : value.username,
+      username: myForm.value.username + '',
+      password: myForm.value.password + '',
+    };
+    this.loginSubscription = this.loginService
+      .login(loginValues.username, loginValues.password)
+      .subscribe(
+        (value: any) => {
+          this.tokenService.saveToken(value.token);
+          const userConnected = {
+            id: value.id,
+            email: value.email,
+            firstName: value.firstName,
+            lastName: value.lastName,
+            gender: value.gender,
+            username: value.username,
+          };
+          this.tokenService.saveUser(userConnected);
+          this.isLoginSuccessful = true;
+          this.dataSharingService.isConnceted.next(true);
+          this.route.navigateByUrl('');
+        },
+        (error: any) => {
+          console.log(error);
+          this.isLoginSuccessful = false;
         }
-        this.tokenService.saveUser(userConnected)
-        this.isLoginSuccessful = true;
-        this.dataSharingService.isConnceted.next(true)
-        this.route.navigateByUrl('');
-      },
-      (error : any) =>{
-        console.log(error)
-        this.isLoginSuccessful = false ;
-      }
-    )
+      );
   }
-
 }
